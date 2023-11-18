2d7921e4a6356badbe7124e0b4dbf78ce15437f9
AndreaDiaz23 <andrea.grafico23@gmail.com>
2023-11-18 08:59:22 -0500

    workone

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBut = document.querySelector('[data-start]');
+const stopBut = document.querySelector('[data-stop]');
 
+let changeColor = null;
+
+startBut.addEventListener('click', startColor);
+stopBut.addEventListener('click', stopColor);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function newColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function startColor() {
+    startBut.disabled = true;
+    changeColor = setInterval(newColor, 1000);
+}
+
+function stopColor() {
+    startBut.disabled = false;
+    clearInterval(changeColor);
+}
